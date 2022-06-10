d9f4f8398c80ef612904df308a19822a81e418e9
seoh2k <seoh2k@gmail.com>
2022-06-10 12:26:20 +0900

    AppConfig 리팩터링 - 목적: 중복 제거, 역할에 따른 구현이 보이도록 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,9 +1,12 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
@@ -11,14 +14,29 @@ public class AppConfig { // 애플리케이션의 전체 동작 방식을 구성
     // 1. 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
     // 2. 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결): MemberServiceImpl에 생성자 주입
     
-    public MemberService memberService() {
+    /*public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository()); // appConfig 객체는 memoryMemberRepository 객체를 생성하고 그 참조값을 memberServiceImpl을 생성하면서 생성자로 전달한다.
         // 클라이언트인 memberSericeImpl 입장에서 보면 의존관계를 마치 외부에서 주입해주는 것 같다고 해서 DI(Dependency Injection) 우리말로 의존관계 주입 또는 의존성 주입이라 한다.
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }*/
+
+    // 리팩터링: 중복 제거, 역할에 따른 구현이 보이도록 변경
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
     }
 
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
 
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository(); // MemoryMemberRepository를 다른 구현체로 변경할 때 한 부분만 변경 가능
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 }
