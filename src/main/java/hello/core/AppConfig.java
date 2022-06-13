ad529be7c117beb4a89456711a7cc70f3e2877c2
seoh2k <seoh2k@gmail.com>
2022-06-13 12:37:42 +0900

    스프링으로 전환 - ApplicationContext를 스프링 컨테이너라 한다. - 스프링 컨테이너는 @Configuration이 붙은 AppConfig를 설정(구성) 정보로 사용한다. @Bean이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다. 이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈이라 한다. - 스프링 빈은 @Bean이 붙은 메서드의 명을 스프링 빈의 이름으로 사용한다.(memberService, orderService) - 스프링 컨테이너를 통해서 필요한 스프링 빈(객체)를 찾아야 한다. 스프링 빈은 applicationContext.getBean() 메서드를 사용해서 찾을 수 있다. - 스프링 컨테이너에 객체를 스프링 빈으로 등록하고, 스프링 컨테이너에서 스프링 빈을 찾아서 사용한다.

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,7 +10,10 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 스프링 기반으로 변경: AppConfig에 설정을 구성한다는 뜻
 public class AppConfig { // 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스
     // 1. 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
     // 2. 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결): MemberServiceImpl에 생성자 주입
@@ -24,19 +28,26 @@ public class AppConfig { // 애플리케이션의 전체 동작 방식을 구성
     }*/
 
     // 리팩터링: 중복 제거, 역할에 따른 구현이 보이도록 변경
+    @Bean // 스프링 기반으로 변경: 스프링 컨테인에 스프링 빈으로 등록
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository(); // MemoryMemberRepository를 다른 구현체로 변경할 때 한 부분만 변경 가능
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        // return new FixDiscountPolicy();
+
+        // 정률 할인 정책으로 변경
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
         // AppConfig 실행
-        AppConfig appConfig = new AppConfig();
+        // appConfig = new AppConfig();
         // MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+        // MemberService memberService = appConfig.memberService();
+    
+        // 스프링 컨테이너 적용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
