1d5997d25706aa05524326fae66233e22926cb66
seoh2k <seoh2k@gmail.com>
2022-08-14 13:20:37 +0900

    스프링 컨테이너와 스프링 빈: 컨테이너에 등록된 모든 빈 조회

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -27,7 +27,8 @@ class ApplicationContextInfoTest {
     void findAllBean(){ // 실행하면 스프링에 등록된 모든 빈 정보를 출력할 수 있다.
         String[] beanDefinitionNames = ac.getBeanDefinitionNames(); // 스프링에 등록된 모든 빈 이름을 조회한다.
         for(String beanDefinitionName : beanDefinitionNames){
-            Object bean = ac.getBean(beanDefinitionName); // 빈 이름으로 빈 객체(인스턴스)를 조회한다.
+            //타입을 모르기 때문에 Object
+            Object bean = ac.getBean(beanDefinitionName); // ac.getBean(): 빈 이름으로 빈 객체(인스턴스)를 조회한다.
             System.out.println("name=" + beanDefinitionName + " object=" + bean);
         }
     }
@@ -37,6 +38,7 @@ class ApplicationContextInfoTest {
     void findApplicationBean(){ // 스프링이 내부에서 사용하는 빈은 제외하고, 내가 등록한 빈만 출력한다.
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for(String beanDefinitionName : beanDefinitionNames) {
+            // 빈에 대한 메타데이터 정보 꺼내기
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
             // Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈, 일반적으로 사용자가 정의한 빈
@@ -45,65 +47,6 @@ class ApplicationContextInfoTest {
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name=" + beanDefinitionName + " object=" + bean);
             }
-        }
-    }
-
-    @Test@DisplayName("빈 이름으로 조회")
-    void findBeanByName(){
-        MemberService memberService = ac.getBean("memberService", MemberService.class); // 스프링 컨테이너에서 스프링 빈을 찾는 가장 기본적인 조회 방법
-        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
-    }
-
-    @Test
-    @DisplayName("이름 없이 타입만으로 조회")
-    void findBeanByType(){
-        MemberService memberService = ac.getBean(MemberService.class);
-        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
-    }
-
-    @Test
-    @DisplayName("구체 타입으로 조회")
-    void findBeanByName2(){
-        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
-        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
-    }
-
-    @Test
-    @DisplayName("빈 이름으로 조회X")
-    void findBeanByNameX(){
-        // ac.getBean("xxxxx", MemberService.class);
-        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxxx", MemberService.class));
-    }
-
-    @Test
-    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
-    void findBeanByTypeDuplicate(){
-        // DiscountPolicy bean = ac.getBean(MemberRepository.class);
-        assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(MemberRepository.class));
-    }
-
-    @Test
-    @DisplayName("특정 타입을 모두 조회하기")
-    void findAllBeanByType(){
-        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
-        for(String key : beansOfType.keySet()){
-            System.out.println("key = " + key + " value = " + beansOfType.get(key));
-        }
-        System.out.println("beansOfType = " + beansOfType);
-        assertThat(beansOfType.size()).isEqualTo(2);
-    }
-
-    @Configuration
-    static class SameBeanConfig {
-
-        @Bean
-        public MemberRepository memberRepository1(){
-            return new MemoryMemberRepository();
-        }
-
-        @Bean
-        public MemberRepository memberRepository2(){
-            return new MemoryMemberRepository();
         }
     }
 }
